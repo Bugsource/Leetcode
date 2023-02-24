186c9b55213d5efa45ed67acd97307e4caf90fcb
harryzhuang <harryzhuang@tencent.com>
2023-02-24 10:56:02 +0800

    二叉树目录创建

---
+++ src/BinaryTree/MaximumDepthOfBinaryTree.java
@@ -0,0 +1,10 @@
+package BinaryTree;
+
+public class MaximumDepthOfBinaryTree {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+}
